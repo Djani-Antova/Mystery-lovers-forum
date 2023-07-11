cdb3332dacac44c6af9da3fcc4dcf03065f677bb
djaniAntova <djaniantova@gmail.com>
2023-07-11 23:06:53 +0300

    Update

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,14 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    CoreModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
