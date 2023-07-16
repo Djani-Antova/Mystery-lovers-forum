17db91574386e6263f71224d4d1e81f978fb5a96
djaniAntova <djaniantova@gmail.com>
2023-07-16 20:57:12 +0300

    Update

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,11 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { HomeComponent } from './home/home.component';
+//import { CoreModule } from './core/core.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
 
   ],
   imports: [
--- my-app/src/app/core/core.module.ts
+++ my-app/src/app/core/core.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent,
-    FooterComponent
+    
+    FooterComponent,
+    NavigationComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    HeaderComponent,
+    NavigationComponent,
     FooterComponent
   ]
 })
