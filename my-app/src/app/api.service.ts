51ae5ebfd5ffd1e1db39659a496e003ec63f2f7e
djaniAntova <djaniantova@gmail.com>
2023-07-20 23:40:23 +0300

    Update api.service.ts

--- my-app/src/app/api.service.ts
+++ my-app/src/app/api.service.ts
@@ -1,20 +1,35 @@
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+ 
+  constructor(private http: HttpClient) {}
+    getPosts() {
+      const { appUrl } = environment;  
 
-  constructor(private http: HttpClient) { }
+      return this.http.get(`${appUrl}/posts`)
+    }
 
-  getPosts(){
-    const { appUrl } = environment
-    return this.http.get(`${appUrl}/posts`)
+    getComments() {
+      const { appUrl } = environment;
+      // const limitFilter = limit ? `?limit=${limit}` : '';
+
+      return this.http.get(`${appUrl}/comments`)
+    }
+
+    // getComments(limit?: number) {
+    //   const { appUrl } = environment;
+    //   const limitFilter = limit ? `?limit=${limit}` : '';
+
+    //   return this.http.get(`${appUrl}/comments${limitFilter}`)
+    // }
   }
-  getComments() {
-    const { appUrl } = environment
-    return this.http.get(`${appUrl}/comments`)
-  }
-}
+
+
+
