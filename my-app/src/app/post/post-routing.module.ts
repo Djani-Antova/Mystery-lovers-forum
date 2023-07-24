10c77c61c75022fef47f3f170299426f77ad3e79
djaniAntova <djaniantova@gmail.com>
2023-07-24 22:30:57 +0300

    Add guards

--- my-app/src/app/post/post-routing.module.ts
+++ my-app/src/app/post/post-routing.module.ts
@@ -3,17 +3,34 @@ import { RouterModule, Routes } from '@angular/router';
 import { NewPostComponent } from './new-post/new-post.component';
 import { CurrentPostComponent } from './current-post/current-post.component';
 import { ForumComponent } from '../forum/forum.component';
+import { AuthActivate } from '../core/guards/authActivate';
 
 
 const routes: Routes = [
-    {path: 'posts', component: ForumComponent},
- 
-  {path: 'add-post', component: NewPostComponent},
-  // {path: 'current-post', component: CurrentPostComponent}
+  {
+    path: 'posts',
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: ForumComponent,
+      },
+      {
+        path: ':postId',
+        component: CurrentPostComponent
+      }
+    ]
+  },
+
+  { path: 'create-post', 
+  component: NewPostComponent,
+  canActivate: [AuthActivate] 
+},
+  
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class PostRoutingModule { }
---
+++ my-app/src/app/user/user.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { User } from '../types/user';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  user: User | undefined;
+  USER_KEY = '[user]'; //Key is specifically used to store and retrieve the user object in localStorage.
+
+  get isLogged(): boolean {
+    return !!this.user;
+  }
+
+  constructor() {
+    try {
+      const localStorageUser = localStorage.getItem(this.USER_KEY) || "";
+      this.user = JSON.parse(localStorageUser)
+    } catch (error) {
+      this.user = undefined;
+    }
+  }
+
+  login(): void {
+    this.user = {
+      email: 'peter@abv.bg',
+      userName: 'Peter',      
+    };
+    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+  };
+  
+  logout(): void {
+    this.user = undefined;
+    localStorage.removeItem(this.USER_KEY);
+  }
+
+}
+
