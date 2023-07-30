2db50ef9079dcfeaa520a0a897108118487d5061
djaniAntova <djaniantova@gmail.com>
2023-07-30 19:44:35 +0300

    Update profile component

--- my-app/src/app/user/profile/profile.component.ts
+++ my-app/src/app/user/profile/profile.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
-
+  userName = 'John Doe';
+  email = 'john.doe@gmail.com';
+  avatarUrl = '../../../assets/user-icon.png'; 
 }
