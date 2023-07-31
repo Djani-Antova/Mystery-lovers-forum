42a4c24fd40092a10997f8d157164ee08ee63bfe
djaniAntova <djaniantova@gmail.com>
2023-07-31 23:11:35 +0300

    Update

--- my-app/src/app/post/current-post/current-post.component.ts
+++ my-app/src/app/post/current-post/current-post.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
+import { Post } from 'src/app/types/post';
 
 @Component({
   selector: 'app-current-post',
   templateUrl: './current-post.component.html',
   styleUrls: ['./current-post.component.css']
 })
-export class CurrentPostComponent {
+export class CurrentPostComponent implements OnInit{
+
+  post: Post | undefined;
+
+  constructor(private apiService: ApiService, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+      this.fetchPost()
+  }
+
+  fetchPost(): void {
+      const id = this.activatedRoute.snapshot.params['postId']
+      this.apiService.getPost(id).subscribe((post) => {
+        this.post = post;
+        console.log(post);
+        
+      }) 
+  }
+
 
 }
