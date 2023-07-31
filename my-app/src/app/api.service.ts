90426178c0ea9e15095a93a773c796d63c41214a
djaniAntova <djaniantova@gmail.com>
2023-07-31 23:12:27 +0300

    Update api.service.ts

--- my-app/src/app/api.service.ts
+++ my-app/src/app/api.service.ts
@@ -1,31 +1,32 @@
-
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Post } from './types/post';
 import { Comment } from './types/comment';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
- 
   constructor(private http: HttpClient) {}
-    getPosts() {
-      const { appUrl } = environment;  
 
-      return this.http.get<Post[]>(`${appUrl}/posts`)
-    }
+  getPost(id: string) {
+    const { appUrl } = environment;
 
-    getComments(limit?: number) {  
-      const { appUrl } = environment;
+    return this.http.get<Post>(`${appUrl}/posts/${id}`);
+  }
+
+  getPosts() {
+    const { appUrl } = environment;
+
+    return this.http.get<Post[]>(`${appUrl}/posts`);
+  }
+
+  getComments(limit?: number) {
+    const { appUrl } = environment;
     //TODO if we want to show only 'number' post
     // const limitFilter = limit ? `?limit=${limit}` : '';
     // return this.http.get(`${appUrl}/comments${limitFilter}`);
-      return this.http.get<Comment[]>(`${appUrl}/comments`);
-    }
+    return this.http.get<Comment[]>(`${appUrl}/comments`);
   }
-
-
-
+}
